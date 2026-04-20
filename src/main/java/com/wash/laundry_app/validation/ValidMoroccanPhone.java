2134edceb0e8c07af50761e94878c1f0a7a75fbd
abde-louali <abdssamadlouali1@icloud.com>
2026-04-20 18:18:16 +0100

    restore deleted validation packages

--- src/main/java/com/wash/laundry_app/validation/ValidMoroccanPhone.java
+++ src/main/java/com/wash/laundry_app/validation/ValidMoroccanPhone.java
@@ -4,12 +4,14 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MoroccanPhoneValidator.class)
 @Documented
 public @interface ValidMoroccanPhone {
     String message() default "Format de numéro de téléphone invalide (Ex: 0612345678 ou +212612345678)";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
